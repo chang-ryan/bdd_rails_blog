6f88bd95d6fc8bac5c4ba7975bb7551676469b65
Ryan Chang <ry4nch4ng@gmail.com>
2016-12-05 07:36:48 +0000

    add some notes

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,13 @@ class Article < ApplicationRecord
   validates :title, :body, presence: true
   
   default_scope { order(created_at: :desc) }
+  
+  # scope :red, -> { where(color: :red) } 
+  #
+  # def self.red
+  #   where(color: 'red')
+  # end
+  #
+  # http://api.rubyonrails.org/classes/ActiveRecord/Scoping/Named/ClassMethods.html
+  
 end
