887e63aff7f93733762f4fdc6df6d5d9758a07d3
Ryan Chang <ry4nch4ng@gmail.com>
2016-12-06 20:00:35 +0000

    signing in use spec finished

--- spec/features/signing_up_users_spec.rb
+++ spec/features/signing_up_users_spec.rb
@@ -3,23 +3,25 @@ require 'rails_helper'
 RSpec.feature "Signing up users" do
   scenario "with valid credentials" do
     visit "/"
+    click_link "Log In"
     click_link "Sign up"
     fill_in "Email", with: "hello@world.com"
     fill_in "Password", with: "password"
-    fill_in "Password confirmation", with: "password"
+    fill_in "Confirmation", with: "password"
     click_button "Sign up"
     
-    expect(page).to have_content("You have successfully signed up.")
+    expect(page).to have_content("You have signed up successfully.")
   end
 
   scenario "with invalid credentials" do
     visit "/"
+    click_link "Log In"
     click_link "Sign up"
     fill_in "Email", with: ""
     fill_in "Password", with: ""
-    fill_in "Password confirmation", with: ""
+    fill_in "Confirmation", with: ""
     click_button "Sign up"
     
-    expect(page).to have_content("errors prevented")
+    expect(page).to have_content("errors prohibited")
   end
 end
