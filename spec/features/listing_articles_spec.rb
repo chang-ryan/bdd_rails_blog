f791865cc6a7e90aeeea1f5cba392e08e01f93bd
Ryan Chang <ry4nch4ng@gmail.com>
2016-12-08 19:25:47 +0000

    hide new article button from non users

--- spec/features/listing_articles_spec.rb
+++ spec/features/listing_articles_spec.rb
@@ -3,14 +3,13 @@ require "rails_helper"
 RSpec.feature "Creating articles" do
   before do
     john = User.create!(email: "john@example.com", password: "password")
-    login_as(john)
     @article1 = john.articles.build(title: "The First Article", body: "Lorem my ipsum, dawg.")
     @article2 = john.articles.build(title: "The Second Article", body: "The body of my second article.")
     @article1.save
     @article2.save
   end
 
-  scenario "User lists all articles" do
+  scenario "with articles created and user not signed in" do
     visit "/"
     
     expect(page).to have_content(@article1.title)
@@ -19,9 +18,10 @@ RSpec.feature "Creating articles" do
     expect(page).to have_content(@article2.body)
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
+    expect(page).to_not have_link("New Article")
   end
   
-  scenario "User has no articles" do
+  scenario "with no articles" do
     Article.delete_all
     visit "/"
     
