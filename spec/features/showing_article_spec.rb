bd22382e26685104d55e3921c18320533813d858
Ryan Chang <ry4nch4ng@gmail.com>
2016-12-08 19:34:41 +0000

    verifies owner for edit/delete

--- spec/features/showing_article_spec.rb
+++ spec/features/showing_article_spec.rb
@@ -2,12 +2,12 @@ require "rails_helper"
 
 RSpec.feature "Creating articles" do
   before do
-    john = User.create!(email: "john@example.com", password: "password")
-    login_as(john)
-    @article = Article.create(title: "The First Article", body: "Lorem my ipsum, dawg.", user: john)
+    @john = User.create!(email: "john@example.com", password: "password")
+    @fred = User.create!(email: "fred@example.com", password: "password")
+    @article = Article.create(title: "The First Article", body: "Lorem my ipsum, dawg.", user: @john)
   end
   
-  scenario "User shows one article" do
+  scenario "with non user of article" do
     visit "/"
     
     click_link @article.title
@@ -15,5 +15,33 @@ RSpec.feature "Creating articles" do
     expect(page).to have_content(@article.title)
     expect(page).to have_content(@article.body)
     expect(current_path).to eq(article_path(@article))
+    expect(page).to_not have_link("Edit Article")
+    expect(page).to_not have_link("Delete Article")
+  end
+  
+  scenario "with non owner of article" do
+    login_as(@fred)
+    visit "/"
+    
+    click_link @article.title
+    
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+    expect(page).to_not have_link("Edit Article")
+    expect(page).to_not have_link("Delete Article")
+  end
+  
+  scenario "with owner of article" do
+    login_as(@john)
+    visit "/"
+    
+    click_link @article.title
+    
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+    expect(page).to have_link("Edit Article")
+    expect(page).to have_link("Delete Article")
   end
 end
